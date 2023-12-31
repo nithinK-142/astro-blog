6b33daf0f30c19899496e600607a0170b491ae99
nithinK-142 <nithin.sagar.359@gmail.com>
2023-12-31 20:04:53 +0530

    update: modified api to fetch search query.

--- src/pages/api/search.json.ts
+++ src/pages/api/search.json.ts
@@ -1,6 +1,7 @@
 import type { APIRoute } from "astro";
+import { getCollection, type CollectionEntry } from "astro:content";
 
-export const GET: APIRoute = ({ url }): Response => {
+export const GET: APIRoute = async ({ url }): Promise<Response> => {
   const query: string | null = url.searchParams.get("query");
 
   if (query === null) {
@@ -17,7 +18,27 @@ export const GET: APIRoute = ({ url }): Response => {
     );
   }
 
-  return new Response(JSON.stringify({ query }), {
+  const allBlogArticles: CollectionEntry<"blog">[] = await getCollection(
+    "blog"
+  );
+
+  const searchResults = allBlogArticles.filter((article) => {
+    const titleMatch: boolean = article.data.title
+      .toLowerCase()
+      .includes(query!.toLowerCase());
+
+    const bodyMatch: boolean = article.body
+      .toLowerCase()
+      .includes(query!.toLowerCase());
+
+    const slugMatch: boolean = article.slug
+      .toLowerCase()
+      .includes(query!.toLowerCase());
+
+    return titleMatch || bodyMatch || slugMatch;
+  });
+
+  return new Response(JSON.stringify(searchResults), {
     status: 200,
     headers: {
       "Content-Type": "application/json",
