2376bd75a06052cfb90da4f299e6ccaf7820ac80
nithinK-142 <nithin.sagar.359@gmail.com>
2024-01-01 18:45:33 +0530

    refactor: Footer content using constants.

--- src/constants.ts
+++ src/constants.ts
@@ -3,3 +3,8 @@ export const SITE_DESCRIPTION =
   "Articles, stories and tutorials from Tech People";
 export const HOMEPAGE_ARTICLE_LIMIT = 6;
 export const ARTICLES_PER_PAGE = 6;
+
+export const FOOTER_DESCRIPTION =
+  "TechPeople is a blog for tech enthusiasts. We publish articles, stories and tutorials about the latest technology trends and advancements.";
+
+export const SOCIALS = ["Email", "Twitter", "Facebook", "Instagram"];
